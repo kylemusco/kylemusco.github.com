5bbd2a6b1147194b3c0ca47f1f83a4b1779288c2
Kyle Musco <kyle@moena.us>
2018-02-15 15:30:35 -0500

    Projects
    Started projects page

---
+++ js/projects.js
@@ -0,0 +1,58 @@
+$(document).ready( function() {
+    // Display 'all' projects by default
+    displayProjects("all");
+});
+
+function displayProjects(tag) {
+    // First, hide all projects
+    $('.project-box').hide();
+
+    // Then show projects with desired tag
+    $('.project-box').each( function() {
+        var tags = $(this).attr('tags');
+        if( tags != undefined ) {
+            if( tags.includes(tag) ) {
+                $(this).show();
+            }
+        }
+    });
+}
+
+function setInactive() {
+    $('.project-nav-item').removeClass("active");
+}
+
+$(document).on('click', '#all', function() {
+    setInactive();
+    displayProjects("all");
+
+    $(this).addClass("active");
+});
+
+$(document).on('click', '#hackathons', function() {
+    setInactive();
+    displayProjects("hackathons");
+    
+    $(this).addClass("active");
+});
+
+$(document).on('click', '#hardware', function() {
+    setInactive();
+    displayProjects("hardware");
+    
+    $(this).addClass("active");
+});
+
+$(document).on('click', '#networking', function() {
+    setInactive();
+    displayProjects("networking");
+    
+    $(this).addClass("active");
+});
+
+$(document).on('click', '#software', function() {
+    setInactive();
+    displayProjects("software");
+    
+    $(this).addClass("active");
+});
